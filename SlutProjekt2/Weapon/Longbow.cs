8ccecc24ce29ddbc272529374be1bbb342b80952
Daniel Taherkhanian <daniel.taherkhanian@elev.ga.ntig.se>
2024-03-10 19:31:09 +0100

    V 0.22 Added more code

--- SlutProjekt2/Weapon/Longbow.cs
+++ SlutProjekt2/Weapon/Longbow.cs
@@ -1,4 +1,16 @@
-﻿public class Longbow
+﻿public class Longbow : RangedWeapon
 {
+    private Random Generator = new Random();
 
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+
+    public Longbow()
+    {
+        weaponName = "Longbow";
+        weaponDamage = Generator.Next(2, 7);
+        weaponCriticalDamage = 5;
+        weaponBlock = Generator.Next(1, 5);
+        weaponAccuracy = 9;
+    }
 }
--- SlutProjekt2/Weapon/Shortbow.cs
+++ SlutProjekt2/Weapon/Shortbow.cs
@@ -1,4 +1,16 @@
-﻿public class Shortbow
+﻿public class Shortbow : RangedWeapon
 {
+    private Random Generator = new Random();
 
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+
+    public Shortbow()
+    {
+        weaponName = "Shortbow";
+        weaponDamage = Generator.Next(1, 5);
+        weaponCriticalDamage = 4;
+        weaponBlock = Generator.Next(1, 3);
+        weaponAccuracy = 7;
+    }
 }
